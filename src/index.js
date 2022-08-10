c32e6e60f29b1e2ac5c51e2fd5ebc17ac7ce404d
FrontHitss <programador.hitss@gmail.com>
2022-08-10 16:45:17 -0500

    Soft-ui-dashboard

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('index');
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.use(express.static('public'));
+//app.use('/static', express.static(__dirname + '/public'));
+
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
+
+app.listen(port, () => {
+    console.log(`Server on port:${port}`);
+});
