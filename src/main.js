b7d543b5fb919b68c2085f8e5f9e7b6685ae37e7
FrontHitss <programador.hitss@gmail.com>
2022-08-12 17:29:31 -0500

    update server

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ var colors = require('colors');
 colors.enable();
 
 app.use(express.static(__dirname + './../public'));
-app.use((req, res, next) => {res.status(404).sendFile(__dirname + './../public/404.html');});
+//app.use((req, res, next) => {res.status(404).sendFile(__dirname + './../public/404.html');});
 
 app.listen(PORT, () => {
     console.log(`Server on PORT:${PORT}`.blue);
